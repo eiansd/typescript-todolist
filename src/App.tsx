371643425ac4c49a82cdc8e989b85b66f1ef7f72
eiansd <joh7727@naver,com>
2023-12-18 16:04:58 +0900

    readme 작성중

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import styled from "styled-components";
+import uuid from "react-uuid";
+import Input from "./components/Input";
+import Todolist from "./components/Todolist";
+
+export type Todo = {
+  id: string;
+  title: string;
+  contents: string;
+  isDone: boolean;
+};
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      id: uuid(),
+      title: "제목1",
+      contents: "내용1",
+      isDone: false,
+    },
+    {
+      id: uuid(),
+      title: "제목2",
+      contents: "내용2",
+      isDone: false,
+    },
+    {
+      id: uuid(),
+      title: "제목3",
+      contents: "내용3",
+      isDone: false,
+    },
+    {
+      id: uuid(),
+      title: "제목4",
+      contents: "내용4",
+      isDone: true,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <StHeader> My Todo List</StHeader>
+      <StMain>
+        <div>
+          <Input todos={todos} setTodos={setTodos} />
+        </div>
+        <div>
+          <Todolist todos={todos} setTodos={setTodos} listIsDone={false} />
+          <Todolist todos={todos} setTodos={setTodos} listIsDone={true} />
+        </div>
+      </StMain>
+    </>
   );
 }
 
 export default App;
+
+const StHeader = styled.div`
+  background-color: #edf5ae;
+  padding: 10px;
+  font-weight: bolder;
+`;
+const StMain = styled.div`
+  background-color: #88b1ab;
+  padding: 10px;
+`;
